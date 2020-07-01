48fb99e6d48a03b4b440c2fe9f202e1dfce1a94d
Muhammad Faiz Ismail <faismail21@gmail.com>
2020-07-01 16:26:52 +0700

    homepage

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,11 +2,9 @@ import React, {Component} from 'react';
 import {StackNavigator} from 'react-navigation';
 import Login from './pages/Login';
 import Homepage from './pages/Homepage';
-import Attendance from './pages/Attendance';
 
 
 export const Pages = StackNavigator({
   Login:{screen:Login,navigationOptions:{ header:null}},
   Homepage:{screen:Homepage,navigationOptions:{ header:null}},
-  Attendance:{screen:Attendance,navigationOptions:{ header:null}},
 })
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,94 +1,105 @@
 import React, { Component } from 'react';
-import { Container, Header,Body, Button, Title, Item, Content, Card, CardItem, Form, Image } from 'native-base';
+import { Container, Header,Body, Button, Title, Item, Content, Card, CardItem, Form, View } from 'native-base';
 import axios from 'axios';
-import {StyleSheet, Text, TouchableOpacity } from 'react-native';
-
+import {StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 
 export default class Homepage extends Component {
 
-  Homepage =()=>
-     {
-        this.props.navigation.navigate('Attendance');
-     }
-
-  render() {
-    return (
-      <Form style={styles.container}>
-          <Text style={styles.TitleText}> Mitracomm Ekasarana </Text>
-
-          <Text style={styles.EmployeeText}> Employee Name </Text>
-
-          <Text style={styles.EmployeeText}> Employee Reg Number </Text>
-          
-          {/* <Image></Image> */}
-
-          <Card style={styles.Card}>
-
-              <TouchableOpacity button onPress={this.Homepage} style={styles.login} >
-                <Text style={styles.loginText}> Attendance </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.login} button onPress={() => alert("This is Card Body")}>
-                <Text style={styles.loginText}> Staff </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.login} button onPress={() => alert("Thanks for CheckIn / Thanks for Today")}>
-                <Text style={styles.loginText}> Options </Text>
-              </TouchableOpacity>
+    render() {
+        return (
+          <Form style={styles.container}>
+              <Text style={styles.TitleText}> Mitracomm Ekasarana </Text>
+    
+              <Text style={styles.EmployeeText}> Employee Name </Text>
+    
+              <Text style={styles.EmployeeText}> Employee Reg Number </Text>
               
-          </Card>
-      </Form>
-    );
-  }
+              <View style={{ flexDirection:'row', marginTop:80,  marginHorizontal: 20, }} >
+                <View style= {{ justifyContent:'space-between', flexDirection:'row', width:'100%' }} >
+                    <View>
+                        <TouchableOpacity style={styles.Menu}>
+                        <Image style={{ width:80, height:80 }} source={require('../images/attendance.png')}></Image>
+                        </TouchableOpacity>
+                        <Text style={styles.EmployeeText}> Attendance </Text>
+                    </View>
+                    <View>
+                        <TouchableOpacity style={styles.Menu}>
+                        <Image style={{ width:80, height:80 }} source={require('../images/staff.png')}></Image>
+                        </TouchableOpacity>
+                        <Text style={styles.EmployeeText}> Staff </Text>
+                    </View>
+                    <View>
+                        <TouchableOpacity style={styles.Menu}>
+                        <Image style={{ width:80, height:80 }} source={require('../images/options.png')}></Image>
+                        </TouchableOpacity>
+                        <Text style={styles.EmployeeText}> Options </Text>
+                    </View>
+                </View>
+              </View>
+
+          </Form>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   
-    container : {
-    backgroundColor:'white',
-    flex: 1,
-    alignItems:'center',
-    justifyContent :'center'
-    },
-
-    TitleText: {
-    fontSize: 30,
-    fontWeight:'bold',
-    color:'black',  
-    textAlign:'center'    
-    },
-
-    EmployeeText: {
-    fontSize: 20,
-    fontWeight:'normal',
-    color:'black',  
-    textAlign:'center'    
-    },
+        container : {
+        backgroundColor:'white',
+        flex: 1,
+        alignItems:'center',
+        justifyContent :'center'
+        },
     
-    Card: {
-    width: 400,
-    height: 400,
-    borderRadius: 10,
-    backgroundColor:'black',
-    marginBottom: 15,
-    alignItems:'center',
-    justifyContent :'center'
-    },
+        TitleText: {
+        fontSize: 30,
+        fontWeight:'bold',
+        color:'black',  
+        textAlign:'center'    
+        },
+    
+        EmployeeText: {
+        fontSize: 20,
+        fontWeight:'normal',
+        color:'black',  
+        textAlign:'center'    
+        },
+        
+        Card: {
+        width: 400,
+        height: 400,
+        borderRadius: 10,
+        backgroundColor:'black',
+        marginBottom: 15,
+        alignItems:'center',
+        justifyContent :'center'
+        },
+    
+        login: {
+        width: 200,    
+        backgroundColor:'skyblue',
+        justifyContent :'center',
+        borderRadius: 25,
+        marginVertical: 5,
+        paddingVertical: 15
+        },
+                  
+        loginText: {
+        fontSize:25,
+        fontWeight:'normal',
+        color:'black',  
+        textAlign:'center'    
+        },
 
-    login: {
-    width: 200,    
-    backgroundColor:'skyblue',
-    justifyContent :'space-between',
-    borderRadius: 25,
-    marginVertical: 5,
-    paddingVertical: 15
-    },
-              
-    loginText: {
-    fontSize:25,
-    fontWeight:'normal',
-    color:'black',  
-    textAlign:'center'    
-    },
-
-});
+        Menu: {
+        width: 80,
+        height: 80,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 18,
+        marginHorizontal: 20,
+        justifyContent:'center',
+        alignItems:'center',
+        }
+    
+    });
